25fbdff174fc0d6035e990222c984b83ccc54991
DimiPetrov <d_petrov@yahoo.com>
2020-10-06 12:35:21 +0300

    samchanges

--- Functions/wrongResult.js
+++ Functions/wrongResult.js
@@ -1,6 +1,6 @@
 function solve(numOne, numTwo, numThree) {
     let result = '';
-    if(numOne >= 0 && numTwo >= 0 && numThree >= 0) {
+    if(numOne >= 0 || numTwo >= 0 || numThree >= 0) {
         result = 'Positive';
     } else if(numOne < 0 && numTwo > 0 && numThree > 0) {
         result = 'Negative';
@@ -18,8 +18,8 @@ function solve(numOne, numTwo, numThree) {
         result = 'Positive';
     } else if((numOne == 0 && numTwo == 0) || (numOne == 0 && numTree == 0) || (numTwo == 0 && numThree == 0)) {
         result = 'Positive';
-    }
+    } 
     console.log(result);
 }
 
-solve(-6, 0, 0)
+solve(-6, 0, 10)
